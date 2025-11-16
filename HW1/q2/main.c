7b41282681671f42fdc4aaff2e8429b17cde4e2c
yavidor <yonatan@avidor.org>
2025-11-16 17:54:03 +0200

    Starting q2

--- HW1/q2/main.c
+++ HW1/q2/main.c
@@ -1,7 +1,18 @@
+#include <stdbool.h>
 #include <stdio.h>
 
 int main(void) {
     int input = 0;
-    if (scanf("%d", &input) == 1) {
+    int result = scanf("%d", &input);
+    if (result == 1) {
+        "YOU FCKED UP"
+    }
+    bool isPositive = 0;
+    if (input > 0) {
+        isPositive = true;
+    } else {
+        isPositive = false;
+    }
+    while (input != 0) {
     }
 }
