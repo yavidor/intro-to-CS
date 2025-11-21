195a369fb97449df7836b729314f89adcc593901
yavidor <yonatan@avidor.org>
2025-11-22 01:22:02 +0200

    gitignore

--- HW1/q2/main.c
+++ HW1/q2/main.c
@@ -3,16 +3,22 @@
 
 int main(void) {
     int input = 0;
-    int result = scanf("%d", &input);
+    char after = 0;
+    int result = scanf("%d%c", &input, &after);
+
     if (result == 1) {
-        "YOU FCKED UP"
+        // "YOU FCKED UP"
+        printf("! result I guess\n");
     }
+    printf("input: %d\nafter: %d\n", input, after);
     bool isPositive = 0;
     if (input > 0) {
         isPositive = true;
     } else {
         isPositive = false;
     }
-    while (input != 0) {
-    }
+    printf("%d\n", isPositive);
+    // while (input != 0) {
+    //     printf("%d", isPositive);
+    // }
 }
