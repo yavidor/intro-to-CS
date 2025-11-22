c2e95c3e929c974f5f4b27ff49d3bd734aa7817d
yavidor <yonatan@avidor.org>
2025-11-22 16:08:39 +0200

    Finished the second question

--- HW1/q2/main.c
+++ HW1/q2/main.c
@@ -3,14 +3,16 @@
 
 int main(void) {
     int input = 0;
-    char suffix = 0;
+    char suffix = 0; // Needed to check if input contains invalid characters
     int output = 0;
-    scanf("%d%c", &input, &suffix);
+    int result = scanf("%d%c", &input, &suffix);
 
-    if (suffix != '\n') {
+    // If additional characters was captured, it must be newlike
+    if (result != 1 && suffix != '\n') {
         printf("Error in input!\n");
     } else {
         while (input != 0) {
+            // Take out the last digit from the input, add it to the output
             output *= 10;
             output += input % 10;
             input /= 10;
