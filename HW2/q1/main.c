2265224c9db198030c970a6bde015e7ba840c1cd
yavidor <yonatan@avidor.org>
2025-12-12 18:22:20 +0200

    Moved to C99

--- HW2/q1/main.c
+++ HW2/q1/main.c
@@ -4,7 +4,28 @@
 
 #include <stdio.h>
 
+#define ALPHABET 26
+#define SIZE 27
+#define A_ASCII 97
+#define MAX_HIST 26
+
+void encrypt(const char text[], const int size, int hist) {
+    char arr[size];
+    arr[size - 1] = '\0';
+    for (int i = 0; i < size - 1; i++) {
+        if (text[i] == ' ') {
+            arr[i] = ' ';
+            continue;
+        }
+        arr[i] = ((text[i] - A_ASCII + hist) % ALPHABET) + A_ASCII;
+    }
+    printf("%s\n", arr);
+}
+
 int main(void) {
-    printf("Hello, World!");
+    char plain[SIZE] = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < MAX_HIST; i++) {
+        encrypt(plain, SIZE, i);
+    }
     return 0;
 }
