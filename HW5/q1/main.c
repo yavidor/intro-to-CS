602606715c53925a8a0f65662e27b8f022173a11
yavidor <yonatan@avidor.org>
2026-01-23 13:25:37 +0200

    HW5

---
+++ HW5/q1/main.c
@@ -0,0 +1,51 @@
+//
+// Created by yavidor on 1/23/2026.
+//
+#include <stdio.h>
+#include <stdbool.h>
+#define NUM_PLATFORMS 3
+#define MAX_FARM_SIZE 1000
+
+bool shouldSwapCapybaras(int pop1, int plat1, int pop2, int plat2) {
+  if (pop1!=pop2)
+    return pop1<pop2;
+  return plat1>plat2;
+}
+
+void swapCapybaras(int popularity[], int platform[], int i, int j){
+  int temp;
+  temp = popularity[i];
+  popularity[i] = popularity[j];
+  popularity[j] = temp;
+  platform[i] = platform[j];
+  platform[j] = platform[i];
+}
+
+void viralCapySort(int popularity[], int platform[], int n){
+
+}
+
+int main(){
+  int n, popularity[MAX_FARM_SIZE], platform[MAX_FARM_SIZE];
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++){
+    scanf("%d", &popularity[i]);
+  }
+  for (int i = 0; i <= n; i++){
+    scanf("%d", &platform[i]);
+  }
+  viralCapySort(popularity, platform, n);
+  for (int i = 0; i < n; i++){
+    printf("%d", popularity[i]);
+    if (i < n - 1)
+      printf(" ");
+  }
+  printf("\n");
+  for (int i = 0; i < n; i++){
+    printf("%d", platform[0]);
+    if (i < n - 1)
+      printf(" ");
+  }
+  printf("\n");
+  return 0;
+}
---
+++ HW5/q2/main.c
@@ -0,0 +1,60 @@
+//
+// Created by yavidor on 1/23/2026.
+//
+#include <stdio.h>
+#include <stdbool.h>
+
+#define N 9
+
+
+// Function to print the board
+void printBoard(int board[N][N]) {
+  // You shouldn't change this function!!
+
+  for (int i = 0; i < N; i++) {
+    if (i % 3 == 0 && i != 0) {
+      printf("---------------------\n");
+    }
+    for (int j = 0; j < N; j++) {
+      if (j % 3 == 0 && j != 0) {
+        printf("| ");
+      }
+      printf("%d ", board[i][j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
+
+// Write your solution in this function.
+// You may (and encourage to) use as many helper functions as you want.
+bool solveSudoku(int board[N][N]) {
+  return false;
+}
+
+
+int main() {
+  // You shouldn't change this function!!
+
+  int board[N][N];
+
+  // Read input directly into the board
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      scanf("%d", &board[i][j]);
+    }
+  }
+
+  printf("Original Sudoku:\n");
+  printBoard(board);
+
+  // If the given board has a solution prints it. Otherwise, print no solution statement.
+  if (solveSudoku(board)) {
+    printf("Solved Sudoku:\n");
+    printBoard(board);
+  } else {
+    printf("No solution exists.\n");
+  }
+
+  return 0;
+}
