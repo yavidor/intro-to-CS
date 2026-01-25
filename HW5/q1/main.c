1b6ddc78d0f24c3dbd1b92a07c4ecdbe5da492be
yavidor <yonatan@avidor.org>
2026-01-25 21:10:28 +0200

    First question

--- HW5/q1/main.c
+++ HW5/q1/main.c
@@ -6,36 +6,71 @@
 #define NUM_PLATFORMS 3
 #define MAX_FARM_SIZE 1000
 
+// Checks if two capys should be swapped
+// pop1: The popularity of the first
+// plat1: The platform of the first
+// pop2: The popularity of the second
+// plat2: The platform of the second
 bool shouldSwapCapybaras(int pop1, int plat1, int pop2, int plat2) {
-    if (pop1 != pop2)
-        return pop1 < pop2;
-    return plat1 > plat2;
+    if (plat1 != plat2) {
+        return plat1 < plat2;
+    }
+    return pop1 > pop2;
 }
 
+// Swaps two capys
+// popularity: The popularity of the capybaras
+// platform: The platforms of the capybaras
+// i: Index of the first capy
+// j: Index of the second capy
 void swapCapybaras(int popularity[], int platform[], int i, int j) {
     int temp;
     temp = popularity[i];
     popularity[i] = popularity[j];
     popularity[j] = temp;
+    temp = platform[i];
     platform[i] = platform[j];
-    platform[j] = platform[i];
+    platform[j] = temp;
 }
 
+// Bubble up one capy array iterations
+// popularity: The popularity of the capybaras
+// platform: The platforms of the capybaras
+// n: length
+void bubble(int popularity[], int platform[], int n) {
+    for (int i = 1; i < n; i++) {
+        if (shouldSwapCapybaras(popularity[i], platform[i], popularity[i - 1],
+                                platform[i - 1])) {
+            swapCapybaras(popularity, platform, i, i - 1);
+        }
+    }
+}
+
+// Sorts the capybaras
+// popularity: The popularity of the capybaras
+// platform: The platforms of the capybaras
+// n: length
 void viralCapySort(int popularity[], int platform[], int n) {
-    printf("%d\n", popularity[0]);
-    printf("%d\n", platform[0]);
-    printf("%d\n", n);
+    while (n > 1) {
+        bubble(popularity, platform, n--);
+    }
+}
+
+// Gets the input from the user
+// popularity: The popularity of the capybaras
+// platform: The platforms of the capybaras
+// n: length
+void getInput(int popularity[], int platform[], int *n) {
+    scanf("%d", n);
+    for (int i = 0; i < *n; i++)
+        scanf("%d", &popularity[i]);
+    for (int i = 0; i < *n; i++)
+        scanf("%d", &platform[i]);
 }
 
 int main() {
     int n, popularity[MAX_FARM_SIZE], platform[MAX_FARM_SIZE];
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &popularity[i]);
-    }
-    for (int i = 0; i <= n; i++) {
-        scanf("%d", &platform[i]);
-    }
+    getInput(popularity, platform, &n);
     viralCapySort(popularity, platform, n);
     for (int i = 0; i < n; i++) {
         printf("%d", popularity[i]);
@@ -44,7 +79,7 @@ int main() {
     }
     printf("\n");
     for (int i = 0; i < n; i++) {
-        printf("%d", platform[0]);
+        printf("%d", platform[i]);
         if (i < n - 1)
             printf(" ");
     }
